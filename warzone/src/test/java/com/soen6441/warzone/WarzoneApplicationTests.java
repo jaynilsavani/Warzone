ae7b71f32b26cd2e9da2a99bd44a027410782d63
gaurangdobariya <dobariyagaurang@gmail.com>
2021-02-10 10:43:44 +0530

    basic structure

---
+++ warzone/src/test/java/com/soen6441/warzone/WarzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen6441.warzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
