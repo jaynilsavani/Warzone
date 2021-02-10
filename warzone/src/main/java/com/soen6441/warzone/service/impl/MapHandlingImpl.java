1f447a90c50b6ae9a955e03df78d89d400d16280
jaynilsavani <jenilsavani009@gmail.com>
2021-02-10 19:34:42 +0530

    refactor: map management class renamed to map handling

---
+++ warzone/src/main/java/com/soen6441/warzone/service/impl/MapHandlingImpl.java
@@ -0,0 +1,19 @@
+package com.soen6441.warzone.service.impl;
+
+import com.soen6441.warzone.service.MapHandlingInterface;
+
+/**
+ * This is the implementation class of MapHandlingInterface having business
+ * logic of map management including create map, edit map and validate map etc.
+ *
+ * @author <a href="mailto:y_vaghan@encs.concordia.ca">Yashkumar Vaghani</a>
+ *
+ */
+public class MapHandlingImpl implements MapHandlingInterface {
+
+    @Override
+    public boolean validateCommand() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
