c0bc3e9d6b93e8926da74d3335e46fbafb2b35a7
jaynilsavani <jenilsavani009@gmail.com>
2021-02-11 14:41:44 +0530

    feat: add continent command validation added

--- warzone/src/main/java/com/soen6441/warzone/service/MapHandlingInterface.java
+++ warzone/src/main/java/com/soen6441/warzone/service/MapHandlingInterface.java
@@ -10,7 +10,25 @@ public interface MapHandlingInterface {
     /**
      * This method will validate all command entered by user
      *
+     * @param p_command contains command string entered by user
      * @return true if command is valid
      */
-    boolean validateCommand();
+    boolean validateCommand(String p_command);
+
+    /**
+     * This function is used to check whether string is empty or not
+     *
+     * @param p_str string passed by user
+     * @return true if string is not null
+     */
+    boolean isNullOrEmpty(String p_str);
+
+    /**
+     * This method will validate the I/O given from GUI or terminal
+     *
+     * @param p_string string you want to validate
+     * @param p_regex regex for validation
+     * @return true if string matches with regex
+     */
+    boolean validateIOString(String p_string, String p_regex);
 }
