57232da535e36b7b6e99162a2811ea70f71d22a6
Gaurang Dobariya <dobariyagaurang@gmail.com>
2021-02-14 17:38:40 +0530

    Added Global test configuration,and solved issue of test in project

--- warzone/src/test/java/com/soen6441/warzone/WarzoneApplicationTests.java
+++ warzone/src/test/java/com/soen6441/warzone/WarzoneApplicationTests.java
@@ -1,13 +1,28 @@
 package com.soen6441.warzone;
 
+import com.soen6441.warzone.serviceImplTest.MapHandlingImplTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * This is the main suite class for textcases. All testcases files for different
+ * services will be mentioned here. From here we can run the test case files.
+ *
+ * @author <a href="mailto:g_dobari@encs.concordia.ca">Gaurang Dobariya</a>
+ * @version 1.0.0
+ * @see com.soen6441.warzone.service
+ * @see com.soen6441.warzone.service.impl
+ */
 @SpringBootTest
+@RunWith(Suite.class)
+@SuiteClasses({MapHandlingImplTest.class})
 class WarzoneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
