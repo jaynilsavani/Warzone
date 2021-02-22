c9e81d71d7e714cd85b8e0430bd4832df835d6ba
manthanmoradiya <manthan.p.moradiya@gmail.com>
2021-02-22 14:39:13 +0530

    new condition added in validmap function

--- warzone/src/main/java/com/soen6441/warzone/service/impl/MapHandlingImpl.java
+++ warzone/src/main/java/com/soen6441/warzone/service/impl/MapHandlingImpl.java
@@ -424,7 +424,7 @@ public class MapHandlingImpl implements MapHandlingInterface {
      */
     @Override
     public boolean validateMap(WarMap p_warMap) {
-        if (d_warMap.getD_continents() == null  || !d_warMap.isD_status()) {
+        if (p_warMap.getD_continents() == null  || !p_warMap.isD_status()) {
             return false;
         }
         boolean l_result = false;
@@ -435,7 +435,20 @@ public class MapHandlingImpl implements MapHandlingInterface {
 
                 // check one or more country is available in map
                 if (!l_countries.isEmpty()) {
-
+                    
+                    //check continent is avilable for each country
+                    List<Integer> l_continentList = new ArrayList();
+                    for(Map.Entry<Integer, Continent> l_entry : d_warMap.getD_continents().entrySet()){
+                        l_continentList.add(l_entry.getValue().getD_continentIndex());
+                    }
+                    for(Map.Entry<Integer, Continent> l_entry : d_warMap.getD_continents().entrySet()){
+                        List<Country> l_countryList = l_entry.getValue().getD_countryList();
+                        for(Country l_country : l_countryList){
+                            if(!l_continentList.contains(l_country.getD_continentIndex())){
+                                l_result = false;
+                            }
+                        }
+                    }                  
                     // check graph is connected or not
                     l_countries = getAvailableCountries(p_warMap);
                     int l_countrySize = l_countries.size();
