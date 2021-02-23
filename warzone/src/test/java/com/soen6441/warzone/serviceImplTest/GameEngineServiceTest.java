2d6ce040fc5538d69d07a75a183905b4eee877af
yashvaghani <yashvaghani44@gmail.com>
2021-02-23 17:42:23 +0530

    refactoring

--- warzone/src/test/java/com/soen6441/warzone/serviceImplTest/GameEngineServiceTest.java
+++ warzone/src/test/java/com/soen6441/warzone/serviceImplTest/GameEngineServiceTest.java
@@ -2,23 +2,18 @@ package com.soen6441.warzone.serviceImplTest;
 
 import com.soen6441.warzone.model.*;
 import com.soen6441.warzone.service.GameEngineService;
-import org.junit.After;
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
-
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * This Class will test business logic of GameEngineService.
@@ -37,20 +32,12 @@ public class GameEngineServiceTest {
 
     @Autowired
     GamePlay d_gamePlay;
-    
+
     @Autowired
     Player d_player;
 
     @Autowired
     WarMap d_warMap;
-
-    /**
-     * This method is used to load SpringBoot Application Context
-     */
-    @Test
-    public void contextLoads() {
-
-    }
 
     public GameEngineServiceTest() {
     }
@@ -61,6 +48,14 @@ public class GameEngineServiceTest {
 
     @AfterClass
     public static void tearDownClass() {
+    }
+
+    /**
+     * This method is used to load SpringBoot Application Context
+     */
+    @Test
+    public void contextLoads() {
+
     }
 
     @Before
@@ -130,16 +125,15 @@ public class GameEngineServiceTest {
     }
 
     @Test
-    public void testExecuteOrder()
-    {
+    public void testExecuteOrder() {
         d_gamePlay.getD_playerList().get(0).setD_noOfArmies(6);
         d_gamePlay.getD_playerList().get(0).setD_currentToCountry("india");
         d_gamePlay.getD_playerList().get(0).setD_currentNoOfArmiesToMove(9);
         d_gamePlay.getD_playerList().get(0).issue_order();
-        Order l_order=d_gamePlay.getD_playerList().get(0).getD_orders().get(0);
+        Order l_order = d_gamePlay.getD_playerList().get(0).getD_orders().get(0);
         ((DeployOrder) l_order).setD_player(d_gamePlay.getD_playerList().get(0));
-        boolean l_actual=l_order.executeOrder();
-        assertEquals(true,l_actual);
+        boolean l_actual = l_order.executeOrder();
+        assertEquals(true, l_actual);
     }
 
 }
