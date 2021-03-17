0155d37b1108a62dee8a8f7421ff5b632473e789
yashvaghani <yashvaghani44@gmail.com>
2021-03-17 16:45:20 +0530

    adding new commands

---
+++ warzone/src/main/java/com/soen6441/warzone/model/NegotiateOrder.java
@@ -0,0 +1,35 @@
+package com.soen6441.warzone.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * This Class is used for The negotiate order Command Three annotations
+ * (Getter,Setter, toString), you can see on the top of the class are lombok
+ * dependencies to automatically generate getter, setter and tostring method in
+ * the code.
+ *
+ * @author <a href="mailto:manthan.p.moradiya@gmail.com">Manthan Moradiya</a>
+ */
+@Getter
+@Setter
+@ToString
+public class NegotiateOrder implements Order{
+
+    /**
+     * player in this order with whom cuurent player wants to negotiate
+     */
+    private String d_playerName;
+    /**
+     * Player in this order
+     */
+    private Player d_player;
+
+    @Override
+    public boolean executeOrder() {
+        return false;
+    }
+}
