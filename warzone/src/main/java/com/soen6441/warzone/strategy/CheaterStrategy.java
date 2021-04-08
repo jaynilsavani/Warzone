c5b20ed2e9700596d36bd0ebd7beba2da11f8edb
vicky911 <patelvicky1995@gmail.com>
2021-04-08 18:09:49 +0530

    Vicky/strategy pattern (#125)
    * added strategy pattern
    
    * JavaDoc Comment to Adapterpattern and Strategy

--- warzone/src/main/java/com/soen6441/warzone/strategy/CheaterStrategy.java
+++ warzone/src/main/java/com/soen6441/warzone/strategy/CheaterStrategy.java
@@ -10,13 +10,23 @@ import lombok.NoArgsConstructor;
 
 /**
  *
- * This Class is used for Implementing CheaterStrategy
+ * This Class is used for Implementing Cheater Strategy of a Player. A NoArgsConstructor
+ * annotation top of the class is a lombok dependency to automatically generate default
+ * Constructor in the code.
  *
  * @author <a href="mailto:g_dobari@encs.concordia.ca">Gaurang Dobariya</a>
  */
 @NoArgsConstructor
 public class CheaterStrategy extends Strategy {
 
+    /**
+     * This is a parameterize constructor used to invoke Constructor of Strategy Class
+     * and it also initializes a list to add specific orders which are allowed in this
+     * strategy
+     *
+     * @param p_gameData GameData Object needed for the player GameData
+     * @param p_player Player Object on which Strategy being Applied
+     */
     public CheaterStrategy(GameData p_gameData, Player p_player) {
         super(p_gameData, p_player);
         List<OrderTypes> l_allowedOrders = new ArrayList<>();
@@ -27,6 +37,9 @@ public class CheaterStrategy extends Strategy {
         this.setD_allowedOrders(l_allowedOrders);
     }
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public Order createOrder() {
         //implement create Order logic 
--- warzone/src/main/java/com/soen6441/warzone/strategy/Strategy.java
+++ warzone/src/main/java/com/soen6441/warzone/strategy/Strategy.java
@@ -12,6 +12,9 @@ import lombok.Setter;
 /**
  *
  * This Abstract class is used as a Basic Structure for various Strategies.
+ * Three annotations (Getter,Setter, NoArgsConstructor), you can see on the
+ * top of the class are lombok dependencies to automatically generate getter,
+ * setter method and default Constructor in the code.
  *
  * @author <a href="mailto:g_dobari@encs.concordia.ca">Gaurang Dobariya</a>
  */
@@ -21,8 +24,10 @@ import lombok.Setter;
 public abstract class Strategy {
 
     /**
+     * This Parameterized constructor is used to initialize to GameData object and Player
+     * object.
      *
-     * @param p_gameData GameData Object needed for the player GameDa
+     * @param p_gameData GameData Object needed for the player GameData
      * @param p_player Player Object on which Strategy being Applied
      */
     public Strategy(GameData p_gameData, Player p_player) {
@@ -46,12 +51,14 @@ public abstract class Strategy {
     public List<OrderTypes> d_allowedOrders;
 
     /**
+     * This method is used to create order specific to strategy.
      *
-     * @return Created ORder according to strategy
+     * @return Created Order according to strategy
      */
     abstract public Order createOrder();
 
     /**
+     * This method is used to get GameData from player.
      *
      * @return Updated GameData By the player
      */
