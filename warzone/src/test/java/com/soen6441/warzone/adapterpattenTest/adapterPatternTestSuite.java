9c6437986f2b5e6ca8f09d63ffc55e66b0e49a96
manthanmoradiya <manthan.p.moradiya@gmail.com>
2021-04-10 14:21:27 +0530

    feat: adapter pattern test cases added

---
+++ warzone/src/test/java/com/soen6441/warzone/adapterpattenTest/adapterPatternTestSuite.java
@@ -0,0 +1,25 @@
+package com.soen6441.warzone.adapterpattenTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({adapterPatrrernTest.class})
+/**
+ *
+ * @author <a href="mailto:manthan.p.moradiya@gmail.com">Manthan Moradiya</a>
+ */
+public class adapterPatternTestSuite {
+
+    /**
+     * This method is used to load Springboot Application Context
+     */
+    @Test
+    public void contextLoads() {
+
+    }
+}
