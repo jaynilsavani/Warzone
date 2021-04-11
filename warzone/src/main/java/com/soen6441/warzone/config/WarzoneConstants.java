9e885dd3cf411187afe0fc885bb7907b78530287
jaynilsavani <jenilsavani009@gmail.com>
2021-04-11 19:57:06 +0530

    minor changes

--- warzone/src/main/java/com/soen6441/warzone/config/WarzoneConstants.java
+++ warzone/src/main/java/com/soen6441/warzone/config/WarzoneConstants.java
@@ -13,8 +13,8 @@ public class WarzoneConstants {
     public static final int DEFAULT_ASSIGN_REINFORCEMENT_DIVIDER = 3;
     public static final int DEFAULT_ASSIGN_REINFORCEMENT_IN_TURN = 5;
     //Constants of Map Editor
-    public static final String MAP_DEF_PATH = "C:\\Users\\SAVANI\\Documents\\Warzone\\warzone\\src\\main\\resources\\maps\\";
-    public static final String GAME_DEF_PATH = "C:\\Users\\SAVANI\\Documents\\Warzone\\games\\";
+    public static final String MAP_DEF_PATH = "./src/main/resources/maps/";
+    public static final String GAME_DEF_PATH = "./games/";
     public static final String NAME = "name";
     public static final String FILES = "[files]";
     public static final String MAP = "[Map]";
