05afcdbaf70ce08aa0b2098195a367bbb889eb1b
Jaynil Savani <76048534+jaynilsavani@users.noreply.github.com>
2021-04-12 10:52:22 +0530

    feat: cheatre player strategy added (#132)

--- warzone/src/main/java/com/soen6441/warzone/strategy/CheaterStrategy.java
+++ warzone/src/main/java/com/soen6441/warzone/strategy/CheaterStrategy.java
@@ -1,28 +1,37 @@
 package com.soen6441.warzone.strategy;
 
+import com.soen6441.warzone.model.Country;
 import com.soen6441.warzone.model.GameData;
 import com.soen6441.warzone.model.Order;
 import com.soen6441.warzone.model.OrderTypes;
 import com.soen6441.warzone.model.Player;
+import com.soen6441.warzone.model.Continent;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  *
- * This Class is used for Implementing Cheater Strategy of a Player. A NoArgsConstructor
- * annotation top of the class is a lombok dependency to automatically generate default
- * Constructor in the code.
+ * This Class is used for Implementing Cheater Strategy of a Player. A
+ * NoArgsConstructor annotation top of the class is a lombok dependency to
+ * automatically generate default Constructor in the code.
  *
  * @author <a href="mailto:g_dobari@encs.concordia.ca">Gaurang Dobariya</a>
  */
 @NoArgsConstructor
+@Getter
+@Setter
 public class CheaterStrategy extends Strategy {
 
+    public static int d_noOfTurns = 10;
+
     /**
-     * This is a parameterize constructor used to invoke Constructor of Strategy Class
-     * and it also initializes a list to add specific orders which are allowed in this
-     * strategy
+     * This is a parameterize constructor used to invoke Constructor of Strategy
+     * Class and it also initializes a list to add specific orders which are
+     * allowed in this strategy
      *
      * @param p_gameData GameData Object needed for the player GameData
      * @param p_player Player Object on which Strategy being Applied
@@ -42,9 +51,78 @@ public class CheaterStrategy extends Strategy {
      */
     @Override
     public Order createOrder() {
-        //implement create Order logic 
+        if (CheaterStrategy.d_noOfTurns > 0) {
+            Set<String> l_allNeighorsName = new HashSet<String>();
+            Set<Country> l_allNeighors = new HashSet<Country>();
+            //Get all Neighbours Countries
+            d_player.getD_ownedCountries().stream().map(l_country -> l_allNeighorsName.addAll(l_country.getD_neighbourCountries())).collect(Collectors.toSet());
 
+            for (String l_neighbour : l_allNeighorsName) {
+                l_allNeighors.add(getCountryByCountryName(l_neighbour));
+            }
+            for (Country l_country : l_allNeighors) {
+                if (d_player.getD_ownedCountries().contains(l_country)) {
+                } else {
+                    //Remove from another player 
+                    for (Player l_player : d_gameData.getD_playerList()) {
+                        if (l_player.getD_ownedCountries().contains(l_country)) {
+                            l_player.getD_ownedCountries().remove(l_country);
+                            break;
+                        }
+                    }
+                    //Add to owned list
+                    d_player.getD_ownedCountries().add(l_country);
+                }
+            }
+            //DoubleArmies in Outsider Countries
+            for (Country l_country : d_player.getD_ownedCountries()) {
+                if (isEnemyNeighbour(l_country)) {
+                    l_country.setD_noOfArmies(l_country.getD_noOfArmies() * 2);
+                }
+            }
+            d_player.getOrderProcessor().setOrderString("Cheater Player Order");
+            d_player.getOrderProcessor().processOrder("cheater", d_gameData);
+            CheaterStrategy.d_noOfTurns--;
+            return null;
+        } else {
+            d_player.getOrderProcessor().setOrderString("done");
+            d_player.getOrderProcessor().processOrder("done", d_gameData);
+        }
         return null;
     }
 
+    public boolean isEnemyNeighbour(Country p_country) {
+        for (Country l_country : d_player.getD_ownedCountries()) {
+            if (!l_country.getD_neighbourCountries().contains(p_country.getD_countryName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This method is used for getting index by name
+     *
+     * @param p_continentMap
+     * @param p_countryName
+     * @return CountryIndex
+     */
+    private Country getCountryByCountryName(String p_countryName) {
+        for (Map.Entry<Integer, Continent> entry : d_gameData.getD_warMap().getD_continents().entrySet()) {
+            Continent l_continent = entry.getValue();
+            //geting all countries from the continents
+            List<Country> l_countryList = l_continent.getD_countryList();
+            if (l_countryList != null) {
+                for (Country l_country : l_countryList) {
+                    if (l_country != null) {
+                        //comparing the county name with given l_country name
+                        if (p_countryName.equalsIgnoreCase(l_country.getD_countryName())) {
+                            return l_country;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
