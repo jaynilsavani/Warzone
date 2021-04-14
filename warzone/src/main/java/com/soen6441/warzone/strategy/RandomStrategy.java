76732a41915d1357974f73878fe6948d0a23e51a
vicky911 <patelvicky1995@gmail.com>
2021-04-14 22:16:30 +0530

    Vicky/strategy pattern (#137)
    * added strategy pattern
    
    * JavaDoc Comment to Adapterpattern and Strategy
    
    * JavaDoc Comment to Strategy Pattern,order and GameEngine
    
    * JavaDoc Comment to Strategy Pattern
    
    * Refactoring Strategy Pattern
    
    * Refactoring Strategy Pattren

--- warzone/src/main/java/com/soen6441/warzone/strategy/RandomStrategy.java
+++ warzone/src/main/java/com/soen6441/warzone/strategy/RandomStrategy.java
@@ -5,8 +5,6 @@ import com.soen6441.warzone.model.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.NoArgsConstructor;
-
 /**
  * This Class is used for Implementing Random Strategy of a Player. A NoArgsConstructor
  * annotation top of the class is a lombok dependency to automatically generate default
@@ -17,17 +15,20 @@ import lombok.NoArgsConstructor;
 
 public class RandomStrategy extends Strategy {
 
-
+    /**
+     * This is a default constructor used to initializes a list to
+     * add specific orders which are allowed in this strategy
+     */
     public RandomStrategy() {
 
         List<OrderTypes> l_allowedOrders = new ArrayList<>();
-        l_allowedOrders.add(OrderTypes.DEPLOY);
-        l_allowedOrders.add(OrderTypes.ADVANCE);
-        l_allowedOrders.add(OrderTypes.AIRLIFT);
-        l_allowedOrders.add(OrderTypes.BOMB);
-        l_allowedOrders.add(OrderTypes.BLOCKADE);
-        l_allowedOrders.add(OrderTypes.DIPLOMACY);
-        this.setD_allowedOrders(l_allowedOrders);
+        l_allowedOrders.add( OrderTypes.DEPLOY );
+        l_allowedOrders.add( OrderTypes.ADVANCE );
+        l_allowedOrders.add( OrderTypes.AIRLIFT );
+        l_allowedOrders.add( OrderTypes.BOMB );
+        l_allowedOrders.add( OrderTypes.BLOCKADE );
+        l_allowedOrders.add( OrderTypes.DIPLOMACY );
+        this.setD_allowedOrders( l_allowedOrders );
     }
 
     /**
@@ -39,7 +40,7 @@ public class RandomStrategy extends Strategy {
      * @param p_player   Player Object on which Strategy being Applied
      */
     public RandomStrategy(GameData p_gameData, Player p_player) {
-        super(p_gameData, p_player);
+        super( p_gameData, p_player );
 
     }
 
@@ -50,87 +51,94 @@ public class RandomStrategy extends Strategy {
     public Order createOrder() {
         Country l_fromCountry;
         int l_noOfArmies, l_index;
-        int a;
+        int l_a;
         if (d_player.getD_orders().size() > 3) {
-            a = generateUniqueRandomNumber(1, this.d_allowedOrders.size() + 1);
+            l_a = generateUniqueRandomNumber( 1, this.d_allowedOrders.size() + 1 );
         } else {
-            a = generateUniqueRandomNumber(1, this.d_allowedOrders.size());
+            l_a = generateUniqueRandomNumber( 1, this.d_allowedOrders.size() );
         }
         List<Country> l_randomCountries = getRandomCountries();
-        if(l_randomCountries==null || l_randomCountries.size()==0 )
-        {
-            a=7;
+        if (l_randomCountries == null || l_randomCountries.size() == 0) {
+            l_a = 7;
         }
-        switch (a) {
+        switch (l_a) {
             case 1:
-                l_noOfArmies = generateUniqueRandomNumber(1, d_player.getD_noOfArmies());
-                d_player.getOrderProcessor().processOrder("deploy " + l_randomCountries.get(0).getD_countryName() + " " + l_noOfArmies, d_gameData);
-                //d_player.setD_issuedNoOfArmies(d_player.getD_issuedNoOfArmies() - l_noOfArmies);
+                l_noOfArmies = generateUniqueRandomNumber( 1, d_player.getD_noOfArmies() );
+                d_player.getOrderProcessor().processOrder( "deploy " + l_randomCountries.get( 0 ).getD_countryName() + " " + l_noOfArmies, d_gameData );
                 break;
             case 2:
-                if(l_randomCountries.size()==2) {
-                    l_noOfArmies = generateUniqueRandomNumber(1, l_randomCountries.get(0).getD_noOfArmies());
-                    d_player.getOrderProcessor().processOrder("advance " + l_randomCountries.get(0).getD_countryName() + " " + l_randomCountries.get(1).getD_countryName() + " " + l_noOfArmies, d_gameData);
+                if (l_randomCountries.size() == 2) {
+                    l_noOfArmies = generateUniqueRandomNumber( 1, l_randomCountries.get( 0 ).getD_noOfArmies() );
+                    d_player.getOrderProcessor().processOrder( "advance " + l_randomCountries.get( 0 ).getD_countryName() + " " + l_randomCountries.get( 1 ).getD_countryName() + " " + l_noOfArmies, d_gameData );
                     break;
                 }
             case 3:
-                if(l_randomCountries.size()==2) {
-                    l_noOfArmies = generateUniqueRandomNumber(1, l_randomCountries.get(0).getD_noOfArmies());
-                    d_player.getOrderProcessor().processOrder("airlift " + l_randomCountries.get(0).getD_countryName() + " " + l_randomCountries.get(1).getD_countryName() + " " + l_noOfArmies, d_gameData);
+                if (l_randomCountries.size() == 2) {
+                    l_noOfArmies = generateUniqueRandomNumber( 1, l_randomCountries.get( 0 ).getD_noOfArmies() );
+                    d_player.getOrderProcessor().processOrder( "airlift " + l_randomCountries.get( 0 ).getD_countryName() + " " + l_randomCountries.get( 1 ).getD_countryName() + " " + l_noOfArmies, d_gameData );
                     break;
                 }
             case 4:
-                List<Country> l_opponentCountries=getOpponentCountries();
-                l_index=generateUniqueRandomNumber(0,l_opponentCountries.size()-1);
-                d_player.getOrderProcessor().processOrder("bomb " + l_opponentCountries.get(l_index).getD_countryName(), d_gameData);
+                List<Country> l_opponentCountries = getOpponentCountries();
+                l_index = generateUniqueRandomNumber( 0, l_opponentCountries.size() - 1 );
+                d_player.getOrderProcessor().processOrder( "bomb " + l_opponentCountries.get( l_index ).getD_countryName(), d_gameData );
                 break;
             case 5:
-                l_index = generateUniqueRandomNumber(0, d_player.getD_ownedCountries().size() - 1);
-                d_player.getOrderProcessor().processOrder("blockade " + d_player.getD_ownedCountries().get(l_index).getD_countryName(), d_gameData);
+                l_index = generateUniqueRandomNumber( 0, d_player.getD_ownedCountries().size() - 1 );
+                d_player.getOrderProcessor().processOrder( "blockade " + d_player.getD_ownedCountries().get( l_index ).getD_countryName(), d_gameData );
                 break;
             case 6:
-                while(true) {
-                l_index = generateUniqueRandomNumber(0, d_gameData.getD_playerList().size() - 1);
-                if(!d_gameData.getD_playerList().get(l_index).getD_playerName().equalsIgnoreCase(d_player.getD_playerName()))
-                        {
-                            break;
-                        }
-                      }
-                d_player.getOrderProcessor().processOrder("negotiate " + d_gameData.getD_playerList().get(l_index).getD_playerName(), d_gameData);
+                while (true) {
+                    l_index = generateUniqueRandomNumber( 0, d_gameData.getD_playerList().size() - 1 );
+                    if (!d_gameData.getD_playerList().get( l_index ).getD_playerName().equalsIgnoreCase( d_player.getD_playerName() )) {
+                        break;
+                    }
+                }
+                d_player.getOrderProcessor().processOrder( "negotiate " + d_gameData.getD_playerList().get( l_index ).getD_playerName(), d_gameData );
                 break;
             case 7:
-                d_player.getOrderProcessor().setOrderString("done");
-                d_player.getOrderProcessor().processOrder("done", d_gameData);
+                d_player.getOrderProcessor().setOrderString( "done" );
+                d_player.getOrderProcessor().processOrder( "done", d_gameData );
                 break;
         }
         return d_player.getOrderProcessor().getOrder();
     }
 
+    /**
+     * This method is used to get list of random countries deploy or attack
+     *
+     * @return list of random countries
+     */
     private List<Country> getRandomCountries() {
         List<Country> l_randomCountries = new ArrayList<Country>();
         List<Country> l_getOpponentCountries = new ArrayList<>();
-        if(d_player.getD_ownedCountries()!=null && d_player.getD_ownedCountries().size()!=0) {
-            int l_index = generateUniqueRandomNumber(0, d_player.getD_ownedCountries().size() - 1);
-            l_randomCountries.add(d_player.getD_ownedCountries().get(l_index));
-            Country l_moveFromCountry = l_randomCountries.get(0);
-            for (Country l_countries : getAvailableCountries(d_gameData.getD_warMap())) {
-                if ((d_player.getD_ownedCountries().contains(l_countries) || l_moveFromCountry.getD_neighbourCountries().contains(l_countries.getD_countryName())) && (l_countries != l_moveFromCountry)) {
-                    l_getOpponentCountries.add(l_countries);
+        if (d_player.getD_ownedCountries() != null && d_player.getD_ownedCountries().size() != 0) {
+            int l_index = generateUniqueRandomNumber( 0, d_player.getD_ownedCountries().size() - 1 );
+            l_randomCountries.add( d_player.getD_ownedCountries().get( l_index ) );
+            Country l_moveFromCountry = l_randomCountries.get( 0 );
+            for (Country l_countries : getAvailableCountries( d_gameData.getD_warMap() )) {
+                if ((d_player.getD_ownedCountries().contains( l_countries ) || l_moveFromCountry.getD_neighbourCountries().contains( l_countries.getD_countryName() )) && (l_countries != l_moveFromCountry)) {
+                    l_getOpponentCountries.add( l_countries );
                 }
             }
             if (l_getOpponentCountries.size() > 1) {
-                l_index = generateUniqueRandomNumber(0, l_getOpponentCountries.size() - 1);
-                l_randomCountries.add(l_getOpponentCountries.get(l_index));
+                l_index = generateUniqueRandomNumber( 0, l_getOpponentCountries.size() - 1 );
+                l_randomCountries.add( l_getOpponentCountries.get( l_index ) );
             }
         }
         return l_randomCountries;
     }
 
+    /**
+     * This method is used to get list of opponent's country
+     *
+     * @return list of opponent country
+     */
     private List<Country> getOpponentCountries() {
         List<Country> l_getOpponentCountries = new ArrayList<>();
-        for (Country l_countries : getAvailableCountries(d_gameData.getD_warMap())) {
-            if ((!d_player.getD_ownedCountries().contains(l_countries))) {
-                l_getOpponentCountries.add(l_countries);
+        for (Country l_countries : getAvailableCountries( d_gameData.getD_warMap() )) {
+            if ((!d_player.getD_ownedCountries().contains( l_countries ))) {
+                l_getOpponentCountries.add( l_countries );
             }
         }
         return l_getOpponentCountries;
--- warzone/src/main/java/com/soen6441/warzone/strategy/Strategy.java
+++ warzone/src/main/java/com/soen6441/warzone/strategy/Strategy.java
@@ -7,16 +7,17 @@ import com.soen6441.warzone.model.Order;
 import com.soen6441.warzone.model.OrderTypes;
 import com.soen6441.warzone.model.Player;
 import com.soen6441.warzone.model.WarMap;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- *
  * This Abstract class is used as a Basic Structure for various Strategies.
  * Three annotations (Getter,Setter, NoArgsConstructor), you can see on the
  * top of the class are lombok dependencies to automatically generate getter,
@@ -30,31 +31,29 @@ import lombok.Setter;
 public abstract class Strategy {
 
     /**
+     * GameData Object needed for the player
+     */
+    public GameData d_gameData;
+    /**
+     * Player Object on which Strategy being Applied
+     */
+    public Player d_player;
+    /**
+     * List of allowed Order
+     */
+    public List<OrderTypes> d_allowedOrders;
+
+    /**
      * This Parameterized constructor is used to initialize to GameData object and Player
      * object.
      *
      * @param p_gameData GameData Object needed for the player GameData
-     * @param p_player Player Object on which Strategy being Applied
+     * @param p_player   Player Object on which Strategy being Applied
      */
     public Strategy(GameData p_gameData, Player p_player) {
         this.d_gameData = p_gameData;
         this.d_player = p_player;
     }
-
-    /**
-     * GameData Object needed for the player
-     */
-    public GameData d_gameData;
-
-    /**
-     * Player Object on which Strategy being Applied
-     */
-    public Player d_player;
-
-    /**
-     * List of allowed Order
-     */
-    public List<OrderTypes> d_allowedOrders;
 
     /**
      * This method is used to create order specific to strategy.
@@ -73,25 +72,25 @@ public abstract class Strategy {
     }
 
     /**
-     *
      * @param p_startNumber starting number of the Series
-     * @param p_endNumber Ending Number of the Series
+     * @param p_endNumber   Ending Number of the Series
      * @return Random Number
      */
     public int generateUniqueRandomNumber(int p_startNumber, int p_endNumber) {
         ArrayList<Integer> l_numbers = new ArrayList<Integer>();
         for (int l_number = p_startNumber; l_number <= p_endNumber; l_number++) {
-            l_numbers.add(l_number);
+            l_numbers.add( l_number );
         }
         if (l_numbers.size() > 0) {
-            Collections.shuffle(l_numbers);
-            return l_numbers.get(0);
+            Collections.shuffle( l_numbers );
+            return l_numbers.get( 0 );
         }
         return p_startNumber;
     }
 
     /**
      * This method is used to get available countries of all continents
+     *
      * @param p_gameMap objects of WarMap model
      * @return list of available countries
      */
@@ -102,7 +101,7 @@ public abstract class Strategy {
         for (Map.Entry<Integer, Continent> l_entry : p_gameMap.getD_continents().entrySet()) {
             if (l_entry.getValue().getD_countryList() != null) {
                 for (Country l_country : l_entry.getValue().getD_countryList()) {
-                    l_countries.add(l_country);
+                    l_countries.add( l_country );
                 }
             }
         }
